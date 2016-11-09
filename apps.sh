3201d0d23a1b2a62f6b6c9fccaf99064f6d9d531
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2016-11-09 23:21:18 +0600

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -4,45 +4,23 @@
 brew install mas
 
 # Install utility apps
-brew cask install appcleaner
 brew cask install flux
 brew cask install hyperdock
-brew cask install kap
-brew cask install macclean
-brew cask install qlcolorcode
-brew cask install qlmarkdown
-brew cask install qlstephen
-brew cask install quitter
-brew cask install rocket
-brew cask install suspicious-package
 mas install 437151949 # Entropy
+mas install 443126292 # TrashMe
 mas install 937984704 # Amphetamine
-mas install 1082170746 # Aware
 mas install 1000397973 # Wallcat
 
 # Install development apps
-brew cask install docker
-brew cask install docker-toolbox
-brew cask install hyper
-brew cask install imageoptim
-brew cask install medis
-brew cask install mongodb
-brew cask install mongohub
-brew cask install postico
-brew cask install postman
 brew cask install sourcetree
 brew cask install visual-studio-code
-brew cask install zeplin
-mas install 412448059 # ForkLift
 
 # Install productivity apps
 brew cask install itsycal
 brew cask install dropbox
-brew cask install rescuetime
-brew cask install shiori
 mas install 692867256 # Simplenote
-mas install 409183694 # Keynote
 mas install 443987910 # 1Password
+mas install 409183694 # Keynote
 mas install 409203825 # Numbers
 mas install 409201541 # Pages
 
@@ -56,10 +34,11 @@ brew cask install whatsapp
 brew cask install skype
 brew cask install slack
 brew cask install telegram
-mas install 918858936 # Airmail
 mas install 409789998 # Twitter
 
 # Install media apps
+brew cask install handbrake
+brew cask install imageoptim
 brew cask install vlc
 brew cask install webtorrent
 mas install 731738567 # Permute
@@ -67,7 +46,6 @@ mas install 597611879 # Radium
 
 # Install other apps
 brew cask install google-photos-backup
-brew cask install send-to-kindle
 brew cask install transmission
 
 # Remove outdated versions from the cellar
