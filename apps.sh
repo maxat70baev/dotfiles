c5656676756b9227a48e05a11c8a9549a8a0b6ef
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2016-11-12 22:43:49 +0600

    Sort apps list in the installation script

--- apps.sh
+++ apps.sh
@@ -10,10 +10,10 @@ brew install mas
 brew cask install flux
 brew cask install hyperdock
 brew cask install onyx
+mas install 1000397973 # Wallcat
 mas install 437151949 # Entropy
 mas install 443126292 # TrashMe
 mas install 937984704 # Amphetamine
-mas install 1000397973 # Wallcat
 
 # Install development apps
 brew cask install sourcetree
@@ -22,23 +22,23 @@ brew cask install visual-studio-code
 # Install productivity apps
 brew cask install itsycal
 brew cask install dropbox
-mas install 692867256 # Simplenote
-mas install 443987910 # 1Password
-mas install 949724400 # MoneyWiz
-mas install 410628904 # Wunderlist
 mas install 409183694 # Keynote
-mas install 409203825 # Numbers
 mas install 409201541 # Pages
+mas install 409203825 # Numbers
+mas install 410628904 # Wunderlist
+mas install 443987910 # 1Password
+mas install 692867256 # Simplenote
+mas install 949724400 # MoneyWiz
 
 # Install news apps
-mas install 880001334 # Reeder
 mas install 568494494 # Pocket
+mas install 880001334 # Reeder
 
 # Install communication apps
 brew cask install messenger
-brew cask install whatsapp
 brew cask install slack
 brew cask install telegram
+brew cask install whatsapp
 mas install 409789998 # Twitter
 
 # Install media apps
@@ -46,8 +46,8 @@ brew cask install handbrake
 brew cask install imageoptim
 brew cask install vlc
 brew cask install webtorrent
-mas install 731738567 # Permute
 mas install 597611879 # Radium
+mas install 731738567 # Permute
 
 # Install other apps
 brew cask install google-photos-backup
