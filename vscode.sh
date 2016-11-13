45b77c65ca9e7be3e9ea4917f99f07b339143a86
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2016-11-13 17:42:54 +0600

    Replace VS Code Ocean theme by Flatland Monokai

--- vscode.sh
+++ vscode.sh
@@ -4,7 +4,7 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension christian-kohler.path-intellisense
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension donjayamanne.githistory
-code --install-extension golf1052.theme-base16-ocean
+code --install-extension gerane.Theme-FlatlandMonokai
 code --install-extension robertohuertasm.vscode-icons
 
 cat > "~/Library/Application Support/Code/User/settings.json" <<EOF
