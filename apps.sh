e07ca364311da3716b275b1cc69268250f12eadb
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2016-12-11 13:10:13 +0600

    feat: install calibre.app 📚

--- apps.sh
+++ apps.sh
@@ -24,8 +24,9 @@ brew cask install sourcetree
 brew cask install visual-studio-code
 
 # Install productivity apps
-brew cask install itsycal
+brew cask install calibre
 brew cask install dropbox
+brew cask install itsycal
 mas install 409183694 # Keynote
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
