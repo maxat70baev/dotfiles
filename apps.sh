d044b86612897e0861119aec417edc19db0ef3d2
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2017-02-02 19:43:22 +0600

    feat(apps.sh): install deezer

--- apps.sh
+++ apps.sh
@@ -54,6 +54,7 @@ brew cask install whatsapp
 mas install 409789998 # Twitter
 
 # Install media apps
+brew cask install deezer
 brew cask install handbrake
 brew cask install imageoptim
 brew cask install vlc
