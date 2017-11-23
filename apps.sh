717b3783c64ed8a26f99ee77bc6059854393ae84
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2017-11-23 11:31:07 +0600

    feat(apps): Quip (MAS) -> Quip (brew cask)

--- apps.sh
+++ apps.sh
@@ -31,10 +31,10 @@ brew cask install visual-studio-code
 brew cask install calibre
 brew cask install dropbox
 brew cask install enpass
-brew cask install google-chrome
 brew cask install google-backup-and-sync
+brew cask install google-chrome
 brew cask install itsycal
-mas install 1003160018 # Quip
+brew cask install quip
 mas install 409183694 # Keynote
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
