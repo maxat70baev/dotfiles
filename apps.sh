9bf3689ef54bfe49041f5a38c4640b05e2776abb
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2017-11-23 14:48:28 +0600

    feat(apps): remove Simplenote

--- apps.sh
+++ apps.sh
@@ -40,7 +40,6 @@ mas install 409201541 # Pages
 mas install 409203825 # Numbers
 mas install 410628904 # Wunderlist
 mas install 560459030 # Yandex.Disk
-mas install 692867256 # Simplenote
 mas install 949724400 # MoneyWiz
 
 # Install news apps
