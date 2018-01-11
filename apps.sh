5107bbb62b649cd63833c97cf8c42297554df055
Maxat Zhetpisbayev <maxat70baev@gmail.com>
2018-01-11 14:22:44 +0600

    feat(apps): install Trello

--- apps.sh
+++ apps.sh
@@ -35,6 +35,7 @@ brew cask install google-backup-and-sync
 brew cask install google-chrome
 brew cask install itsycal
 brew cask install quip
+mas install 1278508951 #Trello
 mas install 409183694 # Keynote
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
